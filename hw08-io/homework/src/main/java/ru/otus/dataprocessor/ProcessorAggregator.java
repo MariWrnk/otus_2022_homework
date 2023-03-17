ed1a9321566c3c707420f775fc589e5e17cc4a50
mvoronko <mvoronko@diasoft.ru>
2023-03-17 21:13:36 +0300

    Домашнее задание №8 - правка

--- hw08-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw08-io/homework/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -3,22 +3,17 @@ package ru.otus.dataprocessor;
 import ru.otus.model.Measurement;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
 
-        // Добавила настройку локали, потому что локально не читаются дробные числа через точку
+        // Добавлена настройка локали, потому что локально не читаются дробные числа через точку
         Locale.setDefault(Locale.ENGLISH);
 
         //группирует выходящий список по name, при этом суммирует поля value
-        TreeMap<String, Double> resultMap = new TreeMap<>();
-
-        data.forEach(m -> {
-            resultMap.computeIfPresent(m.getName(), (k, v) -> v + m.getValue());
-            resultMap.computeIfAbsent(m.getName(), k -> m.getValue());
-        });
-        return resultMap;
+        return new TreeMap<>(data.stream().sorted(Comparator.comparing(Measurement::getName)).collect(Collectors.toMap(Measurement::getName, Measurement::getValue, Double::sum)));
     }
 }
